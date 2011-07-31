22238524f8335ba7d65643c4d9b4ec2a17b78e82
Dima Kukushkin <dima@kukushkin.me>
2011-07-31 17:22:58 +0400

    Add 500 error template

--- project/urls.py
+++ project/urls.py
@@ -18,4 +18,5 @@ if settings.DEBUG:
     urlpatterns += patterns(
         '',
         url(r'^404$', TemplateView.as_view(template_name='404.html')),
+        url(r'^500$', TemplateView.as_view(template_name='500.html')),
     )
