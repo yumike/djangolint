479b53ca62f80b936b1cab0e3143c399883398e1
Natasha Yumatova <natalie.stepina@gmail.com>
2011-08-06 21:09:18 +0400

    Lint's urls moved to this app.

--- project/urls.py
+++ project/urls.py
@@ -3,14 +3,7 @@ from django.conf.urls.defaults import patterns, include, url
 
 
 urlpatterns = patterns('',
-    url(r'^$', 'lint.views.index',
-        name='lint_create'),
-    url(r'^create$', 'lint.views.create',
-        name='lint_report_create'),
-    url(r'^get_status$', 'lint.views.get_status',
-        name='lint_report_get_status'),
-    url(r'^results/(?P<hash>[a-f0-9]{40})$', 'lint.views.results',
-        name='lint_results'),
+    url(r'^', include('lint.urls')),
 )
 
 if settings.DEBUG:
