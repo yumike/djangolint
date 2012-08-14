1b9aad0e891c5e5ce6b344e8bea77e124b553bac
Dima Kukushkin <dima@kukushkin.me>
2012-08-15 00:34:32 +0400

    Replace relative import with absolute one

--- project/lint/management/commands/delete_expired_reports.py
+++ project/lint/management/commands/delete_expired_reports.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
-from ...models import Report
+from lint.models import Report
 
 
 class Command(BaseCommand):
