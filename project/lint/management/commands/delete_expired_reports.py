055b55e7f97800febbd75fe9f67afe2f37462c6e
Dima Kukushkin <dima@kukushkin.me>
2012-08-14 23:53:47 +0400

    Add command for deletion old reports

---
+++ project/lint/management/commands/delete_expired_reports.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+
+from ...models import Report
+
+
+class Command(BaseCommand):
+
+    help = 'Deletes expired reports'
+
+    def handle(self, *args, **kwargs):
+        Report.objects.delete_expired()
