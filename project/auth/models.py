7c5706efda265080e80a4296b3dbbbbad2db56ea
Dima Kukushkin <dima@kukushkin.me>
2012-08-20 22:39:30 +0400

    Add user model to auth app

--- project/auth/models.py
+++ project/auth/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+
+    identificator = models.CharField(max_length=255, unique=True)
+    username = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    access_token = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return self.username
