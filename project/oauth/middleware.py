6bd28370610a50d0b1639e3fe4fdf9e392a5adb9
Dima Kukushkin <dima@kukushkin.me>
2012-08-22 09:53:01 +0400

    Fix missed import

--- project/oauth/middleware.py
+++ project/oauth/middleware.py
@@ -1,16 +1,18 @@
 from django.utils.functional import SimpleLazyObject
-from .models import User
+from .models import User, AnonymousUser
+
+
+def get_user(request):
+    user_id = request.session.get('user_id')
+    if not user_id:
+        return AnonymousUser()
+    try:
+        return User.objects.get(pk=user_id)
+    except User.DoesNotExist:
+        return AnonymousUser()
 
 
 class UserMiddleware(object):
 
     def process_request(self, request):
-        def get_user():
-            user_id = request.session.get('user_id')
-            if not user_id:
-                return AnonymousUser()
-            try:
-                return User.objects.get(pk=user_id)
-            except User.DoesNotExist:
-                return AnonymousUser()
-        request.user = SimpleLazyObject(get_user)
+        request.user = SimpleLazyObject(lambda: get_user(request))
