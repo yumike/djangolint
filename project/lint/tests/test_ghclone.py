3c799fc995a1f7998a38636d39e03307221952a9
Mike Yumatov <mike@yumatov.org>
2012-08-25 14:24:05 +0400

    Fix current ghclone module tests

--- project/lint/tests/test_ghclone.py
+++ project/lint/tests/test_ghclone.py
@@ -17,39 +17,12 @@ class CloneTests(TestCase):
         self.report2 = Report.objects.create(url='https://github.com/xobb1t/notexistentrepo')
         self.report3 = Report.objects.create(url='https://github.com/mirrors/linux')
 
-    def tearDown(self):
-        try:
-            shutil.rmtree(self.report1.get_repo_path())
-        except OSError:
-            pass
-        try:
-            shutil.rmtree(self.report2.get_repo_path())
-        except OSError:
-            pass
-        try:
-            shutil.rmtree(self.report3.get_repo_path())
-        except OSError:
-            pass
-
     def test_clone(self):
-        path1 = self.report1.get_repo_path()
-        clone(self.report1.github_url, path1)
-        self.assertTrue(os.path.exists(path1))
+        with clone(self.report1.github_url) as path:
+            self.assertTrue(os.path.exists(path))
         with self.assertRaises(CloneError):
-            clone(self.report2.github_url, self.report2.get_repo_path())
+            with clone(self.report2.github_url):
+                pass
         with self.assertRaises(CloneError):
-            clone(self.report3.github_url, self.report3.get_repo_path())
-
-
-    def test_files_delete(self):
-        path1 = self.report1.get_repo_path()
-        clone(self.report1.github_url, path1)
-        self.report1.stage = 'done'
-        self.report1.save()
-        self.assertFalse(os.path.exists(path1))
-        self.report1.stage = 'whait'
-        self.report1.save()
-        clone(self.report1.github_url, path1)
-        self.report1.error = 'error'
-        self.report1.save()
-        self.assertFalse(os.path.exists(path1))
+            with clone(self.report3.github_url):
+                pass
